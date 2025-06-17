5cd2a4b2ffedf90e68a7f6b70b4cd6eb324dac72
mrlongruoi <mrlong01012020@gmail.com>
2025-06-17 21:44:41 +0700

    setup project

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function LoginPage() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Welcome Back!</CardTitle>
+        <CardDescription>Login with your Github Email Account</CardDescription>
+      </CardHeader>
+    </Card>
+  );
+}
