982067689e4f5bc82081c95e6011f4348aa46291
mrlongruoi <mrlong01012020@gmail.com>
2025-06-18 00:36:10 +0700

    02: Create Login UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { ThemeToggle } from "@/components/ui/themeToggle";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1 className="text-2xl font-bold text-red-500">Helo</h1>
+      <ThemeToggle/>
     </div>
   );
 }
