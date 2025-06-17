ec8cb05050a642e32c2576dc5e9bb9a6c771a6cd
mrlongruoi <mrlong01012020@gmail.com>
2025-06-18 02:28:36 +0700

    03: Setup Authentication & Database

---
+++ app/(auth)/login/_components/LoginForm.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { GithubIcon, Loader } from "lucide-react";
+import { toast } from "sonner";
+import { useTransition } from "react";
+
+export function LoginForm() {
+    const [githubPending, startGithubTransition] = useTransition();
+  async function signInWithGithub() {
+    startGithubTransition(async () => {
+      await authClient.signIn.social({
+        provider: "github",
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Singed in with Github, you will be redirected...");
+          },
+          onError: (error) => {
+            toast.error("Internal Server Error");
+          },
+        },
+      });
+    });
+  }
+    return (
+        <Card>
+              <CardHeader>
+                <CardTitle className="text-xl">Welcome Back!</CardTitle>
+                <CardDescription>Login with your Github Email Account</CardDescription>
+              </CardHeader>
+              <CardContent className="flex flex-col gap-4">
+                <Button
+                  disabled={githubPending}
+                  onClick={signInWithGithub}
+                  className="w-full"
+                  variant="outline"
+                >
+                  {githubPending ? (
+                    <>
+                      <Loader className="size-4 animate-spin" />
+                      <span>Loading...</span>
+                    </>
+                  ) : (
+                    <>
+                      <GithubIcon className="size-4" />
+                      Sign in with Github
+                    </>
+                  )}
+                </Button>
+                <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                  <span className="relative z-10 bg-card px-2 text-muted-foreground">
+                    Or continue with
+                  </span>
+                </div>
+                <div className="grid gap-3">
+                  <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="email@example.com"
+                      required
+                    />
+                  </div>
+                  <Button>Continue with Email</Button>
+                </div>
+              </CardContent>
+            </Card>
+    )
+}
