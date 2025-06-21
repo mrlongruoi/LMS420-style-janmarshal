38b1c95bcad22835831612b09a8435e3654bf274
mrlongruoi <mrlong01012020@gmail.com>
2025-06-21 15:18:55 +0700

    09: Course Creation

--- components/sidebar/site-header.tsx
+++ components/sidebar/site-header.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from "../ui/themeToggle";
 
 export function SiteHeader() {
   return (
-    <header className="flex h-[--header-height] shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-[--header-height]">
+    <header className="flex h-[--header-height] shrink-0 items-center gap-2 border-b pb-1 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-[--header-height]">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
         <Separator
@@ -12,7 +12,7 @@ export function SiteHeader() {
           className="mx-2 data-[orientation=vertical]:h-4"
         />
         <h1 className="text-base font-medium">MrlongLMS.</h1>
-        <div className="ml-auto flex items-center gap-2">
+        <div className="ml-auto flex items-center gap-2 mb-1">
           <ThemeToggle />
         </div>
       </div>
---
+++ lib/zodSchemas.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+export const courseLevels = ["Beginner", "Intermediate", "Advanced"] as const;
+export const courseStatus = ["Draft", "Published", "Archived"] as const;
+export const courseCategories = [
+  'Development',
+  'Buisness',
+  'Finance',
+  'IT & Software',
+  'Office Productivity',
+  'Personal Development',
+  'Design',
+  'Marketing',
+  'Health & Fitness',
+  'Music',
+  'Teaching & Academics',
+] as const;
+
+export const courseSchema = z.object({
+  title: z
+    .string()
+    .min(3, { message: "Tiêu đề phải dài ít nhất 3 ký tự" })
+    .max(100, { message: "Tiêu đề phải dài tối đa 100 ký tự" }),
+  description: z
+    .string()
+    .min(3, { message: "Mô tả phải dài ít nhất 3 ký tự" }),
+  fileKey: z.string().min(1, { message: "File là bắt buộc" }),
+  price: z.coerce
+    .number()
+    .min(1, { message: "Giá phải là một số dương" }),
+  duration: z.coerce
+    .number()
+    .min(1, { message: "Thời gian phải ít nhất 1 giờ" })
+    .max(500, { message: "Thời gian phải tối đa 500 giờ" }),
+  level: z.enum(courseLevels, { message: "Cấp độ là bắt buộc" }),
+  category: z.enum(courseCategories, { message: "Danh mục là bắt buộc" }),
+  smallDescription: z
+    .string()
+    .min(3, { message: "Mô tả ngắn phải dài ít nhất 3 ký tự" })
+    .max(200, {
+      message: "Mô tả ngắn phải dài tối đa 200 ký tự",
+    }),
+  slug: z
+    .string()
+    .min(3, { message: "Slug phải dài ít nhất 3 ký tự" }),
+  status: z.enum(courseStatus, { message: "Trạng thái là bắt buộc" }),
+});
+
+export type CourseSchemaType = z.infer<typeof courseSchema>;
